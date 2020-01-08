b9d50fac4d037dc8f22f6d78841db9d3dcf49172
erygreat <ery991172821@gmail.com>
2020-01-08 19:32:17 +0800

    stash

--- utils/telnet.py
+++ utils/telnet.py
@@ -1,4 +1,6 @@
+import logging
 import telnetlib
+from time import sleep
 
 from scrapycw.utils.exception import ScrapycwException
 
@@ -9,13 +11,14 @@ class ScrapycwTelnetException(ScrapycwException):
 
 class Telnet:
 
-    def __init__(self, host, port=0, username=None, password=None):
+    def __init__(self, host, port=0, username=None, password=None, log_level=0):
         self.host = host
         self.port = port
         self.username = username
         self.password = password
         self.__conn = None
         self.err_message = None
+        self.log_level = log_level
 
     def connect(self):
         # try:
@@ -23,20 +26,21 @@ class Telnet:
         # except ConnectionRefusedError as e:
         #     self.err_message = e
         #     return False
+        self.__conn.set_debuglevel(self.log_level)
         if self.username:
             self.command("Username:", self.username)
         if self.username and self.password:
-            self.command("Password: ", self.password)
+            self.command("Password:", self.password)
         return True
 
     def command(self, flag=None, content=""):
-        print(111)
         if self.__conn is None:
             raise ScrapycwTelnetException("Telnet don't connect, please connect it")
+        data = None
         if flag:
             data = self.__conn.read_until(flag.encode())
         self.__conn.write(content.encode() + b"\n")
-        return data
+        return data if data is not None else None
 
     def close(self):
         self.__conn.close()
@@ -44,9 +48,9 @@ class Telnet:
 
 
 if __name__ == "__main__":
-    telnet = Telnet("127.0.0.1", 6023, "scrapy", 'cd0e226506773a8e')
+    telnet = Telnet("127.0.0.1", 6023, "scrapy", 'be2447b3dbb59139', log_level=logging.DEBUG)
     if telnet.connect():
-        telnet.command(">>>", "hello")
+        # telnet.command(content="hello")
         telnet.close()
     else:
         print(telnet.err_message)
