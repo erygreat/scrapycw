6cf6b60e0ff140714a20f9c9ee976dc317322fb9
erygreat <ery991172821@gmail.com>
2021-06-19 17:57:31 +0800

    修复 django 启动后包查找路径问题

--- scrapycw/django_manage.py
+++ scrapycw/django_manage.py
@@ -20,5 +20,5 @@ def main():
 
 
 if __name__ == '__main__':
-    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     main()
