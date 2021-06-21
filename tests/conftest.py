bf77bd1775f72f721497f4d17c23486fbbaa7070
erygreat <ery991172821@gmail.com>
2021-06-21 20:04:26 +0800

    修复Windows上执行测试代码时，加载路径问题

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import os
 import sys
+import platform
 
 pytest_plugins = "pytester"
 current_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if platform.system() == "Windows":
+    current_dir = current_dir.replace("\\", "\\\\")
 sys.path.insert(0, current_dir)
