446a8682f623a70e6c8c5ff2d54e13c1bf0e682e
erygreat <ery991172821@gmail.com>
2021-06-30 13:44:46 +0800

    修改crawl测试用例

---
+++ tests/pytest_run_subprocess/test_crawl.py
@@ -0,0 +1,19 @@
+from scrapycw.utils.scpraycw import init_django_env
+from scrapycw.commands.crawl import Command
+
+
+class Dict(dict):
+    __setattr__ = dict.__setitem__
+    __getattr__ = dict.__getitem__
+
+
+def pytest_crawl_spiders():
+    init_django_env()
+    from scrapycw.web.app.models import SpiderJob
+    opts = Dict()
+    opts['project'] = "default"
+    opts['spargs'] = {}
+    result = Command().run(["baidu"], opts)
+    model = SpiderJob.objects.filter(job_id=result.data['job_id']).get()
+    assert(model is not None)
+    assert(model.job_id == result.data['job_id'])
--- tests/test_crawl.py
+++ tests/test_crawl.py
@@ -1,17 +1,15 @@
-from scrapycw.utils.scpraycw import init_django_env
-from scrapycw.commands.crawl import Command
+from _pytest.config import ExitCode
+from tests.conftest import current_dir
 
-class Dict(dict):
-    __setattr__ = dict.__setitem__
-    __getattr__ = dict.__getitem__
 
-def test_all_spiders():
-    init_django_env()
-    from scrapycw.web.app.models import SpiderJob
-    opts = Dict()
-    opts['project'] = "default"
-    opts['spargs'] = {}
-    result = Command().run(["baidu"], opts)
-    model = SpiderJob.objects.filter(job_id=result.data['job_id']).get()
-    assert(model is not None)
-    assert(model.job_id == result.data['job_id'])
+def test_crawl(testdir):
+    testdir.makepyfile("""
+        import os
+        import sys
+        sys.path.insert(0, "{}")
+
+        from tests.pytest_run_subprocess.test_crawl import pytest_crawl_spiders
+        pytest_crawl_spiders()
+    """.format(current_dir))
+    r = testdir.runpytest_subprocess()
+    assert(r.ret == ExitCode.NO_TESTS_COLLECTED)
