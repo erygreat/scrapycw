c56284dd0dd2d46da8e5500e069afb1e7d6104f5
erygreat <ery991172821@gmail.com>
2021-07-08 18:09:48 +0800

    修改 setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version=version,
     description="一个web监控scrapy的工具",
     long_description="""
-        一个通过web监控和操作scrapy爬虫程序的监控工具。可以通过该工具启动、关闭、操作爬虫程序，也可以通过他观察爬虫的运行情况
+        Scrapycw是一个Scrapy监控程序，你可以通过命令行或者web服务的方式监控Scrapy爬虫运行情况，以及进行运行爬虫等常用操作。
     """,
     install_requires=[
         "Django==3.1.12",
@@ -26,16 +26,19 @@ setup(
     packages=find_packages(exclude=('tests', 'tests.*')),
     author='erygreat',
     author_email='ery991172821@gmail.com',
-    keywords='scrapy web monitor',
+    keywords='Scrapy Web Monitor',
     url='https://github.com/erygreat/scrapycw',
     license="BSD",
     # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Development Status :: 3 - Alpha',
-        "Framework :: Django :: 3.0",
-        "License :: OSI Approved :: Python Software Foundation License",
+        "Framework :: Django :: 3.1",
+        "Framework :: Pytest",
+        "Framework :: Scrapy",
+        "License :: OSI Approved :: BSD License",
         "Natural Language :: Chinese (Simplified)",
         "Operating System :: MacOS",
+        "Operating System :: Microsoft",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3"
     ],
